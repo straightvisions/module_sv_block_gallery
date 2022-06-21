3ec6e9b2d63ff274ee995c937ae2c81a66a60141
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-21 17:48:18 +0200

    improve scripts handling

--- sv_block_gallery.php
+++ sv_block_gallery.php
@@ -93,19 +93,4 @@
 
 			return $this;
 		}
-		public function enqueue_scripts(): sv_block_gallery {
-			if(!$this->has_block_frontend('gallery')){
-				return $this;
-			}
-
-			if(!is_admin()){
-				$this->load_settings()->register_scripts();
-			}
-
-			foreach($this->get_scripts() as $script){
-				$script->set_is_enqueued();
-			}
-
-			return $this;
-		}
 	}
